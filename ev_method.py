90173b1299e3a4ee302b646aa31d6d49cdf33a44
DanielRose2310 <danieldev2310@gmail.com>
2023-04-07 01:06:24 +0300

    first commit

---
+++ ev_method.py
@@ -0,0 +1,21 @@
+import networkx as nx
+
+def k(spectrum, threshold = 0.9):
+    total = sum(abs(spectrum))
+    if total == 0.0:
+        return len(spectrum)
+    i_total = 0.0
+    for i in range(len(spectrum)):
+        i_total += spectrum[i]
+        if i_total / total >= threshold:
+            return i + 1
+    return len(spectrum)
+
+def ev_method(G1,G2,threshold = 0.9):
+    l1 = nx.spectrum.laplacian_spectrum(G1)
+    l2 = nx.spectrum.laplacian_spectrum(G2)
+    k1 = k(l1)
+    k2 = k(l2)
+    k = min(k1, k2,threshold)
+    distance = sum((l1[:k] - l2[:k])**2)
+    return distance
